f164dc99c9ee5977b99032d50cd67a087c037115
Devin Ketchum <dkketch@vt.edu>
2022-03-05 12:16:35 -0500

    initial

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const PORT = 3000;
+const express = require('express');
+const server = express();
+
+const morgan = require('morgan');
+server.use(morgan('dev'));
+
+server.use(express.json());
+
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+
+const { client } = require('./db');
+client.connect();
+
+server.listen(PORT, () => {
+    console.log('The server is up on port', PORT)
+});
+
+server.use((req, res, next) => {
+    console.log("<____Body Logger START____>");
+    console.log(req.body);
+    console.log("<____Body Logger END____>");
+
+    next();
+});
